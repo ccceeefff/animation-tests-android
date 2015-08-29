4f5fdbcca13f7b68b95b0022cfc97c9c1481d3be
Cef Ramirez <cef.ramirez@gmail.com>
2015-08-28 17:06:36 -0700

    adding descriptions for each transition

--- app/src/main/java/com/phunware/materialanimationtest/activity/DetailActivity.java
+++ app/src/main/java/com/phunware/materialanimationtest/activity/DetailActivity.java
@@ -89,9 +89,6 @@ public class DetailActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/phunware/materialanimationtest/activity/MainActivity.java
+++ app/src/main/java/com/phunware/materialanimationtest/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -47,23 +48,15 @@ public class MainActivity extends ActionBarActivity implements BasicItemListAdap
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        boolean result = super.onPrepareOptionsMenu(menu);
 
         if(menu.findItem(R.id.action_anim) != null){
 
             mTransitionSpinner = (Spinner)menu.findItem(R.id.action_anim).getActionView();
             SpinnerAdapter adapter = ArrayAdapter.createFromResource(this, R.array.transitions, R.layout.item_spinner);
             mTransitionSpinner.setAdapter(adapter);
-
-            result = true;
         }
 
-        return result;
+        return true;
     }
 
     @Override
@@ -74,11 +67,42 @@ public class MainActivity extends ActionBarActivity implements BasicItemListAdap
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_details) {
+            showTransitionDetails();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showTransitionDetails(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(mTransitionSpinner.getSelectedItem().toString());
+        switch (mTransitionSpinner.getSelectedItemPosition()){
+            case 0: // default
+                builder.setMessage(R.string.default_transition_details);
+                break;
+            case 1: // slide in
+                builder.setMessage(R.string.slidein_transition_details);
+                break;
+            case 2: // zoom in
+                builder.setMessage(R.string.zoomin_transition_details);
+                break;
+            case 3: // cross fade
+                builder.setMessage(R.string.crossfade_transition_details);
+                break;
+            case 4: // scale up
+                builder.setMessage(R.string.scaleup_transition_details);
+                break;
+            case 5: // shared elements
+                builder.setMessage(R.string.sharedelements_transition_details);
+                break;
+            case 6: // reveal
+                builder.setMessage(R.string.reveal_transition_details);
+                break;
+        }
+        builder.setPositiveButton("OK", null);
+        builder.create().show();
     }
 
     @Override
